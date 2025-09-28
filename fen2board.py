9af822f84f993733210ddfba8df186f6ca5e99bc
James L. Elia <james.elia@yale.edu>
2025-09-28 13:18:44 -0400

    remove testing lines

--- fen2board.py
+++ fen2board.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 import chess
-from pgn2fen import pgn_to_fen
-
 
 def fen_to_board(fen_array: np.array) -> np.array:
     """Given a NumPy array of FEN strings, returns a NumPy array of python-chess Board objects."""
@@ -13,5 +11,3 @@ def fen_to_board(fen_array: np.array) -> np.array:
         board_array.append(chess.Board(fen))
 
     return np.array(board_array)
-    
-print(fen_to_board(pgn_to_fen('./test.pgn')))
