134c6ab80da489255fcaaaca2eef63891eda1071
James L. Elia <james.elia@yale.edu>
2025-09-28 12:30:27 -0400

    cleaned variable names

--- pgn2fen.py
+++ pgn2fen.py
@@ -8,8 +8,8 @@ def pgn_to_fen(pgn_file: str | Path) -> np.array:
     """Given a chess game PGN file, returns a NumPy array of FEN position strings for each move in the game."""
 
     # open provided .pgn file using chess.pgn
-    pgn_file = open(pgn_file)
-    game = chess.pgn.read_game(pgn_file)
+    pgn_game = open(pgn_file)
+    game = chess.pgn.read_game(pgn_game)
 
     # initialize board position and fen array
     board = game.board()
@@ -17,7 +17,6 @@ def pgn_to_fen(pgn_file: str | Path) -> np.array:
 
     # for each position, append to array and progress one move
     for move in game.mainline_moves():
-
         fen_array.append(board.fen())
         board.push(move)
     fen_array.append(board.fen()) # append final position outside of loop
