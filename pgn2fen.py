d0ff981523e9e8a5f05433b6a4953db615a7cb4f
James L. Elia <james.elia@yale.edu>
2025-09-28 12:27:09 -0400

    Add pgn2fen script for converting PGN to array of FEN strings

--- pgn2fen.py
+++ pgn2fen.py
@@ -1,15 +1,25 @@
+import numpy as np
 from pathlib import Path
 
-import chess as ch
+import chess.pgn
 
 
-def pgn_to_fen(pgn: str | Path) -> list:
-    """Given a chess game PGN file, returns a list of FEN position strings one for each move in the game."""
+def pgn_to_fen(pgn_file: str | Path) -> np.array:
+    """Given a chess game PGN file, returns a NumPy array of FEN position strings for each move in the game."""
 
-    raise NotImplementedError
+    # open provided .pgn file using chess.pgn
+    pgn_file = open(pgn_file)
+    game = chess.pgn.read_game(pgn_file)
 
-    pgn = open(pgn)game = ch.pgn.read_game(pgn)
+    # initialize board position and fen array
+    board = game.board()
+    fen_array = []
 
-    print(game.board(pgn))
+    # for each position, append to array and progress one move
+    for move in game.mainline_moves():
 
-pgn_to_fen('./test.pgn')
+        fen_array.append(board.fen())
+        board.push(move)
+    fen_array.append(board.fen()) # append final position outside of loop
+    
+    return np.array(fen_array)
